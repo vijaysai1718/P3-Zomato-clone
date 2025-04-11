4e797404bfb0514268b9e5e666fd08fd2c3ef79f
vijaysai1718 <79537773+vijaysai1718@users.noreply.github.com>
2025-04-11 22:43:32 +0530

    Update Installation.sh

--- Jenkins-Cluster/Installation.sh
+++ Jenkins-Cluster/Installation.sh
@@ -95,3 +95,8 @@ VALIDATE $? "helm installation"
  apt install prometheus prometheus-node-exporter -y
  systemctl enable --now prometheus 
 systemctl status prometheus
+#helm installation
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update
+kubectl create namespace prometheus
+helm install prometheus prometheus-community/prometheus --namespace prometheus
